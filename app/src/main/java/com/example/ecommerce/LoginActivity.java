ac72ec4a0c43b30fc4e108d54bf6e2357f8d6e7a
Alexandru Nastase <nalexandru27@yahoo.com>
2021-04-06 18:59:12 +0300

    Added Login page that is connected to HomeActivity

--- app/src/main/java/com/example/ecommerce/LoginActivity.java
+++ app/src/main/java/com/example/ecommerce/LoginActivity.java
@@ -1,14 +1,110 @@
 package com.example.ecommerce;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.ecommerce.Model.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText inputPhoneNumber;
+    private EditText inputPassword;
+    private Button loginButton;
+    private ProgressDialog loadingBar;
+
+    private String parentDbName = "Users";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inputPhoneNumber = (EditText) findViewById(R.id.login_phone_number_input);
+        inputPassword = (EditText) findViewById(R.id.login_password_input);
+        loginButton = (Button) findViewById(R.id.login_btn);
+        loadingBar = new ProgressDialog(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser() {
+        String phone = inputPhoneNumber.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "Please write your phone", Toast.LENGTH_LONG).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please write your password", Toast.LENGTH_LONG).show();
+        } else {
+            loadingBar.setTitle("Login Account");
+            loadingBar.setMessage("Please wait, while we are checking the credentials");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            AllowAccessToAccount(phone, password);
+        }
+    }
+
+    private void AllowAccessToAccount(String phone, String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.child(parentDbName).child(phone).exists()){
+
+                    Users usersData = snapshot.child(parentDbName).child(phone).getValue(Users.class);
+
+                    if (usersData.getPhone().equals(phone)){
+                        if (usersData.getPassword().equals(password)){
+                            Toast.makeText(LoginActivity.this, "logged successfully", Toast.LENGTH_SHORT);
+                            loadingBar.dismiss();
+
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+//                        else{
+//                            loadingBar.dismiss();
+//                            Toast.makeText(LoginActivity.this,"Bad credentials", Toast.LENGTH_SHORT);
+//                        }
+                    }
+//                    else{
+//                        loadingBar.dismiss();
+//                        Toast.makeText(LoginActivity.this,"Bad credentials", Toast.LENGTH_SHORT);
+//                    }
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Account with this " + phone + "number do not exist", Toast.LENGTH_SHORT);
+                    loadingBar.dismiss();
+                    Toast.makeText(LoginActivity.this,"Bad credentials", Toast.LENGTH_SHORT);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ecommerce/RegisterActivity.java
+++ app/src/main/java/com/example/ecommerce/RegisterActivity.java
@@ -1,10 +1,29 @@
 package com.example.ecommerce;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.hardware.input.InputManager;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,6 +31,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText inputName;
     private EditText inputPhoneNumber;
     private EditText inputPassword;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +42,80 @@ public class RegisterActivity extends AppCompatActivity {
         inputName = (EditText) findViewById(R.id.register_username_input);
         inputPassword = (EditText) findViewById(R.id.register_password_input);
         inputPhoneNumber = (EditText) findViewById(R.id.register_phone_number_input);
+        loadingBar = new ProgressDialog(this);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateAccount();
+            }
+        });
+    }
+
+    private void CreateAccount() {
+        String name = inputName.getText().toString();
+        String phone = inputPhoneNumber.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(this, "Please write your name", Toast.LENGTH_LONG).show();
+        } else if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "Please write your phone", Toast.LENGTH_LONG).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please write your password", Toast.LENGTH_LONG).show();
+        } else {
+            loadingBar.setTitle("Create Account");
+            loadingBar.setMessage("Please wait, while we are checking the credentials");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            ValidatePhoneNumber(name, phone, password);
+        }
+    }
+
+    private void ValidatePhoneNumber(String name, String phone, String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!(snapshot.child("Users").child(phone).exists())) {
+                    HashMap<String, Object> userdataMap = new HashMap<>();
+                    userdataMap.put("phone", phone);
+                    userdataMap.put("password", password);
+                    userdataMap.put("name", name);
+
+                    RootRef.child("Users").child(phone).updateChildren(userdataMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterActivity.this, "Congratulations, your account has been created", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(RegisterActivity.this, "Network error, please try again", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                } else {
+                    Toast.makeText(RegisterActivity.this, "This phone already exists.", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Please try again using another phone number", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }

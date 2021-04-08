bf4ca13966b908b71fc6d131180e2ce84b2e57a4
Alexandru Nastase <nalexandru27@yahoo.com>
2021-04-09 00:18:43 +0300

    Added Home Activity, used RecyclerView for listing products. Also added Logout button functionality.

--- app/src/main/java/com/example/ecommerce/LoginActivity.java
+++ app/src/main/java/com/example/ecommerce/LoginActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ecommerce.Model.Users;
+import com.example.ecommerce.Prevalent.Prevalent;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -116,6 +117,7 @@ public class LoginActivity extends AppCompatActivity {
                               loadingBar.dismiss();
 
                               Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                              Prevalent.currentOnlineUser = usersData;
                               startActivity(intent);
                           }
                         }

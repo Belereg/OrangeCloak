bd632fe6fb06fd256c13b8f6c7e41375f236cc1a
Alex <alexandru.nastase@my.fmi.unibuc.ro>
2021-04-12 09:49:56 +0300

    Added a deleting product functionality from admin's end

--- app/src/main/java/com/example/ecommerce/AdminMaintainProductsActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminMaintainProductsActivity.java
@@ -26,7 +26,7 @@ import okhttp3.internal.cache.DiskLruCache;
 
 public class AdminMaintainProductsActivity extends AppCompatActivity {
 
-    private Button applyChangesBtn;
+    private Button applyChangesBtn, deleteBtn;
     private EditText name, price, description;
     private ImageView imageView;
 
@@ -46,15 +46,39 @@ public class AdminMaintainProductsActivity extends AppCompatActivity {
         price = findViewById(R.id.product_price_maintain);
         description = findViewById(R.id.product_description_maintain);
         imageView = findViewById(R.id.product_image_maintain);
+        deleteBtn = findViewById(R.id.delete_product_btn);
 
         displaySpecificProductInfo();
 
         applyChangesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 applyChanges();
             }
         });
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteThisProduct();
+            }
+        });
+    }
+
+    private void deleteThisProduct() {
+
+        productsRef.removeValue().addOnCompleteListener((new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                Intent intent = new Intent(AdminMaintainProductsActivity.this, AdminCategoryActivity.class);
+                startActivity(intent);
+                finish();
+
+                Toast.makeText(AdminMaintainProductsActivity.this, "The product is deleted successfully!", Toast.LENGTH_SHORT).show();
+            }
+        }));
     }
 
 
